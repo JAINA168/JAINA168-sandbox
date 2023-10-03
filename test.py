83bc495cd5b0c9e5257f4333b5643d5ec66c5f48
JAINA168 <111339277+JAINA168@users.noreply.github.com>
2023-10-03 09:18:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,49 @@
-sed -i 's/pa_matillion_master\.ksh PALIGN DEV\(.*\) PFZALGN_DEV PFZALGN_DEV/pa_matillion_master.ksh PALIGN TEST\1 PFZALGN_TEST PFZALGN_TEST/g' "${jilFile}"
+#!/usr/bin/env groovy
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.UUID
+
+def call(Map config) {
+    def requestId = generateRequestID()
+
+    // ... (your previous code for generating email content)
+
+    // Generate JSON data
+    def jsonParams = [
+        "project_name": env.JOB_NAME,
+        "branch": env.BRANCH_NAME,
+        "project_path": env.WORKSPACE,
+        "job_url": env.JOB_DISPLAY_URL,
+        "build_id": env.BUILD_ID,
+        "build_number": env.BUILD_NUMBER,
+        "build_result": currentBuild.result,
+        "build_parameters": params,
+        "requestid": "${requestId}"
+    ]
+
+    // Convert JSON data to string
+    def jsonString = groovy.json.JsonOutput.toJson(jsonParams)
+
+    // Save JSON data to a file
+    def fileToUpload = "${WORKSPACE}/output.json"
+    writeFile file: fileToUpload, text: jsonString
+
+    // Upload file to S3
+    s3Upload(
+        consoleLogLevel: 'INFO',
+        dontSetBuildResultOnFailure: false,
+        dontWaitForConcurrentBuildCompletion: false,
+        entries: [[
+            bucket: 'mydevopstest', // Replace with your actual S3 bucket name
+            sourceFile: fileToUpload,
+            flatten: true
+        ]],
+        pluginFailureResultConstraint: 'FAILURE'
+    )
+
+    // ... (your previous code for sending email)
+
+    // Rest of your code
+}
+
+// Other functions (generateRequestID, formatTimestamp, getParametersTable) remain the same
