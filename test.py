0991d25b661a6c011ee92fdca38ec060c47c95ab
JAINA168 <111339277+JAINA168@users.noreply.github.com>
2023-10-04 10:50:03 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,72 +1,44 @@
 #!/usr/bin/env groovy
 
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.UUID
-
 def call(Map config) {
     // Your existing code...
 
-    // Function to process JSON data and upload to S3
-    def processAndUploadJSON() {
-        def requestId = generateRequestID()
+    // Function to upload log file to S3 with custom file name
+    def uploadLogFileToS3() {
+        def s3Bucket = 'mydevopstest'
+        def s3Path = 'build-logs/'
 
-        def projectNameSegments = env.JOB_NAME.split('/')
-        def projectName = projectNameSegments[-2] // Assuming the project/pipeline name is the second last segment
+        def projectNameSegments = currentBuild.fullProjectName.split('/')
+        def sfa = projectNameSegments[-3]
+        def Pfizer_Align_UI = projectNameSegments[-2]
+        def branch = projectNameSegments[-1]
 
-        def fileName = "${projectName}_${env.BUILD_NUMBER}_${env.BRANCH_NAME}_${requestId}.json"
+        def logFileName = "${sfa}_${Pfizer_Align_UI}_${BUILD_NUMBER}_${branch}_${requestId}.txt"
+        def localLogFilePath = "${WORKSPACE}/${logFileName}"
+        
+        // Echo the log file name
+        echo "Log File Name: ${logFileName}"
 
-        def jsonParams = [
-            "project_name": env.JOB_NAME,
-            "branch": env.BRANCH_NAME,
-            "project_path": env.WORKSPACE,
-            "job_url": env.JOB_DISPLAY_URL,
-            "build_id": env.BUILD_ID,
-            "build_number": env.BUILD_NUMBER,
-            "build_result": currentBuild.result,
-            "build_parameters": params,
-            "requestid": requestId
-        ]
+        // Perform your build steps...
 
-        // Convert JSON data to string
-        def jsonString = groovy.json.JsonOutput.toJson(jsonParams)
+        // Upload log file to S3 with custom file name
+        sh "touch ${localLogFilePath}"
+        // Add logic to write log content to the local file if needed
 
-        // Save JSON data to a file with the specified name
-        def fileToUpload = "${env.WORKSPACE}/${fileName}"
-        writeFile file: fileToUpload, text: jsonString
-
-        // Upload JSON file to S3
-        s3Upload(
-            consoleLogLevel: 'INFO',
-            dontSetBuildResultOnFailure: false,
-            dontWaitForConcurrentBuildCompletion: false,
-            entries: [
-                [bucket: 'mydevopstest', excludedFile: '', flatten: false, gzipFiles: false, keepForever: false,
-                    managedArtifacts: false, noUploadOnFailure: false, selectedRegion: 'us-east-1',
-                    showDirectlyInBrowser: false, sourceFile: "*${fileName}", storageClass: 'STANDARD', uploadFromSlave: false,
-                    useServerSideEncryption: false]
-            ],
-            pluginFailureResultConstraint: 'FAILURE',
-            profileName: 'devops-test',
-            userMetadata: []
-        )
+        s3Upload consoleLogLevel: 'INFO', dontSetBuildResultOnFailure: false, 
+                dontWaitForConcurrentBuildCompletion: false, 
+                entries: [[bucket: s3Bucket, excludedFile: '', flatten: false, 
+                gzipFiles: false, keepForever: false, managedArtifacts: false, 
+                noUploadOnFailure: false, selectedRegion: 'us-east-1', 
+                showDirectlyInBrowser: false, sourceFile: localLogFilePath, 
+                storageClass: 'STANDARD', uploadFromSlave: false, 
+                useServerSideEncryption: false]], 
+                pluginFailureResultConstraint: 'FAILURE', 
+                profileName: 'devops-test', userMetadata: []
     }
 
-    if ((env.BRANCH_NAME != 'dev') || (config.Email_Alert == 'Yes')) {
-        // Existing code...
+    // Call the function within the script map
+    uploadLogFileToS3()
 
-        // Call the function to process and upload JSON data
-        processAndUploadJSON()
-
-        // More existing code...
-    }
-    // More existing code...
-}
-
-def generateRequestID() {
-    // Existing code...
-}
-
-def getParametersTable() {
-    // Existing code...
+    // Your existing code...
 }
