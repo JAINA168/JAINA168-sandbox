5463bd919b0165f27ff9828c332d83aab9f75f1a
JAINA168 <111339277+JAINA168@users.noreply.github.com>
2023-10-04 10:57:10 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ def call(Map config) {
     // Your existing code...
 
     // Function to upload log file to S3 with custom file name
-    def uploadLogFileToS3() {
+    def uploadLogToS3() {
         def s3Bucket = 'mydevopstest'
         def s3Path = 'build-logs/'
 
@@ -13,32 +13,34 @@ def call(Map config) {
         def Pfizer_Align_UI = projectNameSegments[-2]
         def branch = projectNameSegments[-1]
 
-        def logFileName = "${sfa}_${Pfizer_Align_UI}_${BUILD_NUMBER}_${branch}_${requestId}.txt"
-        def localLogFilePath = "${WORKSPACE}/${logFileName}"
+        def job_split = "jobs/${sfa}/jobs/${Pfizer_Align_UI}/branches/${branch}/builds"
+        def final_job = "${JENKINS_HOME}/${job_split}/${BUILD_NUMBER}/log"
         
-        // Echo the log file name
-        echo "Log File Name: ${logFileName}"
+        def newFileName = "${sfa}_${Pfizer_Align_UI}_${BUILD_NUMBER}_${branch}_DeploymentReqNum.txt"
 
-        // Perform your build steps...
+        // Echo the job URL and log file path
+        echo "Job URL: ${job_split}"
+        echo "Log File Path: ${final_job}"
+        echo "Workspace: ${WORKSPACE}"
 
-        // Upload log file to S3 with custom file name
-        sh "touch ${localLogFilePath}"
-        // Add logic to write log content to the local file if needed
+        // Rename log file to custom name
+        sh "mv ${final_job} ${WORKSPACE}/${newFileName}"
 
-        s3Upload consoleLogLevel: 'INFO', dontSetBuildResultOnFailure: false, 
-                dontWaitForConcurrentBuildCompletion: false, 
-                entries: [[bucket: s3Bucket, excludedFile: '', flatten: false, 
-                gzipFiles: false, keepForever: false, managedArtifacts: false, 
-                noUploadOnFailure: false, selectedRegion: 'us-east-1', 
-                showDirectlyInBrowser: false, sourceFile: localLogFilePath, 
-                storageClass: 'STANDARD', uploadFromSlave: false, 
-                useServerSideEncryption: false]], 
-                pluginFailureResultConstraint: 'FAILURE', 
+        // Upload renamed log file to S3
+        s3Upload consoleLogLevel: 'INFO', dontSetBuildResultOnFailure: false,
+                dontWaitForConcurrentBuildCompletion: false,
+                entries: [[bucket: s3Bucket, excludedFile: '', flatten: false,
+                gzipFiles: false, keepForever: false, managedArtifacts: false,
+                noUploadOnFailure: false, selectedRegion: 'us-east-1',
+                showDirectlyInBrowser: false, sourceFile: newFileName,
+                storageClass: 'STANDARD', uploadFromSlave: false,
+                useServerSideEncryption: false]],
+                pluginFailureResultConstraint: 'FAILURE',
                 profileName: 'devops-test', userMetadata: []
     }
 
     // Call the function within the script map
-    uploadLogFileToS3()
+    uploadLogToS3()
 
     // Your existing code...
 }
