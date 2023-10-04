4ea9c57a02742ffc30f281821be5045be7b03753
JAINA168 <111339277+JAINA168@users.noreply.github.com>
2023-10-04 10:36:14 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,6 +11,11 @@ def call(Map config) {
     def processAndUploadJSON() {
         def requestId = generateRequestID()
 
+        def projectNameSegments = env.JOB_NAME.split('/')
+        def projectName = projectNameSegments[-2] // Assuming the project/pipeline name is the second last segment
+
+        def fileName = "${projectName}_${env.BUILD_NUMBER}_${env.BRANCH_NAME}_${requestId}.json"
+
         def jsonParams = [
             "project_name": env.JOB_NAME,
             "branch": env.BRANCH_NAME,
@@ -26,8 +31,8 @@ def call(Map config) {
         // Convert JSON data to string
         def jsonString = groovy.json.JsonOutput.toJson(jsonParams)
 
-        // Save JSON data to a file
-        def fileToUpload = "${env.WORKSPACE}/output.json"
+        // Save JSON data to a file with the specified name
+        def fileToUpload = "${env.WORKSPACE}/${fileName}"
         writeFile file: fileToUpload, text: jsonString
 
         // Upload JSON file to S3
@@ -38,7 +43,7 @@ def call(Map config) {
             entries: [
                 [bucket: 'mydevopstest', excludedFile: '', flatten: false, gzipFiles: false, keepForever: false,
                     managedArtifacts: false, noUploadOnFailure: false, selectedRegion: 'us-east-1',
-                    showDirectlyInBrowser: false, sourceFile: '*json', storageClass: 'STANDARD', uploadFromSlave: false,
+                    showDirectlyInBrowser: false, sourceFile: "*${fileName}", storageClass: 'STANDARD', uploadFromSlave: false,
                     useServerSideEncryption: false]
             ],
             pluginFailureResultConstraint: 'FAILURE',
@@ -65,56 +70,3 @@ def generateRequestID() {
 def getParametersTable() {
     // Existing code...
 }
-
-
-
-
-#!/usr/bin/env groovy
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.UUID
-
-def call(Map config) {
-    def requestId = generateRequestID()
-
-    // ... (your previous code for generating email content)
-
-    // Generate JSON data
-    def jsonParams = [
-        "project_name": env.JOB_NAME,
-        "branch": env.BRANCH_NAME,
-        "project_path": env.WORKSPACE,
-        "job_url": env.JOB_DISPLAY_URL,
-        "build_id": env.BUILD_ID,
-        "build_number": env.BUILD_NUMBER,
-        "build_result": currentBuild.result,
-        "build_parameters": params,
-        "requestid": "${requestId}"
-    ]
-
-    // Convert JSON data to string
-    def jsonString = groovy.json.JsonOutput.toJson(jsonParams)
-
-    // Save JSON data to a file
-    def fileToUpload = "${WORKSPACE}/output.json"
-    writeFile file: fileToUpload, text: jsonString
-
-    // Upload file to S3
-    s3Upload(
-        consoleLogLevel: 'INFO',
-        dontSetBuildResultOnFailure: false,
-        dontWaitForConcurrentBuildCompletion: false,
-        entries: [[
-            bucket: 'mydevopstest', // Replace with your actual S3 bucket name
-            sourceFile: fileToUpload,
-            flatten: true
-        ]],
-        pluginFailureResultConstraint: 'FAILURE'
-    )
-
-    // ... (your previous code for sending email)
-
-    // Rest of your code
-}
-
-// Other functions (generateRequestID, formatTimestamp, getParametersTable) remain the same
